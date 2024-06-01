752895bc908e2e048b380e0042cead4a9e84c9d5
Tristan Bren V. Ragiles <161691130+Ragiles-TristanBren-BSIT32E1@users.noreply.github.com>
2024-06-01 14:22:29 +0800

    POKEMON

---
+++ pokemonact2/pokemonact2/Program.cs
@@ -0,0 +1,31 @@
+using pokemonact2.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<PokemonController>(client =>
+{
+    client.BaseAddress = new Uri("https://pokeapi.co/api/v2/pokemon");
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Pokemon}/{action=Index}/{id?}");
+
+app.Run();
